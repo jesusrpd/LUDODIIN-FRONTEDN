1cc08629000be117367b171c1ad38568ddd670cf
noejasiel <55604384+noejasiel@users.noreply.github.com>
2022-07-05 00:56:58 -0500

    components folder

---
+++ pages/home.jsx
@@ -0,0 +1,7 @@
+import hero from "../components/hero/hero";
+
+const home = () => {
+  return <h1 className="bg-black">soy home</h1>;
+};
+
+export default home;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import home from "./home";
 
 export default function Home() {
-  return (
-    <div className='bg-black'>
-      <p className='text-white'>funciona</p>
-    </div>
-  )
+  return <home />;
 }
